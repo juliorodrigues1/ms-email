ee1e1917e2fc2e58a995b681a3dcb48bb2b596cc
Julio Cesar <juliosilva.rodrigues@ac.gov.br>
2023-05-04 08:51:44 -0500

    Criando metodo no serviço para envio de email

--- src/main/java/br/gov/ac/msemail/models/EmailModel.java
+++ src/main/java/br/gov/ac/msemail/models/EmailModel.java
@@ -2,11 +2,13 @@ package br.gov.ac.msemail.models;
 
 import br.gov.ac.msemail.enums.StatusEmail;
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Entity
+@Data
 public class EmailModel implements Serializable {
     private static  final long serialVersionUID = 1L;
 
--- src/main/java/br/gov/ac/msemail/services/EmailService.java
+++ src/main/java/br/gov/ac/msemail/services/EmailService.java
@@ -1,8 +1,16 @@
 package br.gov.ac.msemail.services;
 
+import br.gov.ac.msemail.enums.StatusEmail;
+import br.gov.ac.msemail.models.EmailModel;
 import br.gov.ac.msemail.repositories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
@@ -10,5 +18,28 @@ public class EmailService {
     @Autowired
     EmailRepository emailRepository;
 
+    @Autowired
+    private JavaMailSender emailSender;
 
+    @Value("${spring.mail.username}")
+    private String from;
+
+
+    public EmailModel sendEmail(EmailModel emailModel) {
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(this.from);
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getBody());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        }catch (MailException e){
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        }finally {
+            return emailRepository.save(emailModel);
+        }
+    }
 }
