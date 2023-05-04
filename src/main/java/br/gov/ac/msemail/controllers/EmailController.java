5b715a365974b5093b609e4c1a4b598bce932b36
Julio Cesar <juliosilva.rodrigues@ac.gov.br>
2023-05-04 08:16:10 -0500

    Criando metodo para solicitar envio de email

--- src/main/java/br/gov/ac/msemail/controllers/EmailController.java
+++ src/main/java/br/gov/ac/msemail/controllers/EmailController.java
@@ -1,7 +1,16 @@
 package br.gov.ac.msemail.controllers;
 
+import br.gov.ac.msemail.dtos.EmailDto;
+import br.gov.ac.msemail.models.EmailModel;
 import br.gov.ac.msemail.services.EmailService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +18,14 @@ public class EmailController {
 
     @Autowired
     EmailService emailService;
+
+
+    @PostMapping("/sending-mail")
+    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto){
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+    }
+
 }
